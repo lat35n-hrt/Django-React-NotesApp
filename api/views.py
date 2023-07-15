e137bff74e4de406a5b5ba95ef25aa91cebb2ee1
Michael White <hrt666.cs@gmail.com>
2023-07-15 23:49:03 +0900

    feat: add getNotes in views.py and urls.py

--- api/views.py
+++ api/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('Our API', safe=False)
+
+    routes = []
+
+    return Response(routes)
+    # return JsonResponse('Our API', safe=False)
+
+@api_view(['GET'])
+def getNotes(request):
+    return Response('NOTES')
 
