e338997f7980fc0e6bc65e276a578a2acc00bc74
Michael White <hrt666.cs@gmail.com>
2023-08-12 09:16:05 +0900

    feat: add corsheaders in INSTALLED_APPS of settingspy

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
-    path('notes/', views.getNotes, name="notes"),
-        path('notes/<str:pk>', views.getNote, name="notes"),
+    path('api/notes/', views.getNotes, name="notes"),
+    path('api/notes/<str:pk>', views.getNote, name="note"),
 ]
