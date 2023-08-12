9eb1d8ff9438532f7adc231fc92b373847ba0e52
Michael White <hrt666.cs@gmail.com>
2023-08-12 13:54:32 +0900

    debug: fix port number allowance

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -126,6 +126,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3001", 
+# ]
+
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000", 
+    "http://localhost:3000",     
+    "http://localhost:3001", 
 ]
