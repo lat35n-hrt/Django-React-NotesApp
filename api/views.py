6d80af194500e7703e32c7636ba1bed70e6e071f
Michael White <hrt666.cs@gmail.com>
2023-08-28 15:20:00 +0900

    feat: add updateNote in view.py

--- api/views.py
+++ api/views.py
@@ -26,3 +26,15 @@ def getNote(request, pk):
     notes = Note.objects.get(id=pk)
     note_serializer = NoteSerializer(notes, many=False)
     return Response(note_serializer.data)
+
+@api_view(['PUT'])
+def updateNote(request, pk):
+    data = request.data
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(instance=note, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
