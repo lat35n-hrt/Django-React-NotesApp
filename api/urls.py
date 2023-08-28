bf474f80159c145ff2e8fbf4129fc6a263f0d477
Michael White <hrt666.cs@gmail.com>
2023-08-28 15:23:59 +0900

    feat: add path for update in urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('api/notes/', views.getNotes, name="notes"),
+    path('api/notes/<str:pk>/update', views.updateNote, name="update-note"),
     path('api/notes/<str:pk>/', views.getNote, name="note"),
 ]
