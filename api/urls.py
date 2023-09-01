448ab5161a6547280b12cf8de3acefaeb3bbe6b4
Michael White <hrt666.cs@gmail.com>
2023-09-01 12:12:18 +0900

    feat: add a path for createNote

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('api/notes/', views.getNotes, name="notes"),
+    path('api/notes/create', views.createNote, name="create-note"),
     path('api/notes/<str:pk>/update', views.updateNote, name="update-note"),
     path('api/notes/<str:pk>/delete', views.deleteNote, name="delete-note"),
     path('api/notes/<str:pk>/', views.getNote, name="note"),
