dcbfa29a403aafbe6e88c4e9b492fd6247796971
Michael White <hrt666.cs@gmail.com>
2023-09-01 09:11:23 +0900

    feat: add a path for deleteNote in urls.py

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('api/notes/', views.getNotes, name="notes"),
     path('api/notes/<str:pk>/update', views.updateNote, name="update-note"),
+    path('api/notes/<str:pk>/delete', views.deleteNote, name="delete-note"),
     path('api/notes/<str:pk>/', views.getNote, name="note"),
 ]
